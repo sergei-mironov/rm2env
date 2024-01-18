d38ed98cf3ceab51a6a0327ac8be6a84f8c5c5d3
Sergei Mironov <sergei.v.mironov@proton.me>
2024-01-18 15:28:27 +0400

    Add nixconfig.sh local config script

---
+++ sh/nixconfig.sh
@@ -0,0 +1,8 @@
+rmset RM_SSH remarkable
+rmset RM_VPSSSH vps
+rmset RM_XOCHITL $HOME/.xochitl
+rmset RM_VPSRPORT_A 4349
+rmset RM_VPSRPORT_B 4350
+RM_GUIARGS="--width=800 --window-icon=./remarkable.ico --title=RemarkableSync"
+RM_THUMBNAILS="nix-disabled"
+
